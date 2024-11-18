21cc923e64e22de72c90a5864f1b0a50616ea684
Dahlia Walters <Dahlia.Walters@doh.wa.gov>
2024-11-18 11:49:34 -0800

    Consolidate ID cleaning code
    Implement Marcela's suggestions for making code more efficient and outputting a cleaned file.

--- scripts/load_files.R
+++ scripts/load_files.R
@@ -69,18 +69,14 @@ summary_tsv <- read.delim(summary_tsv_files[1])
 
 #DATA CLEANING
 #BigBacter sometimes adds a _T1 to the samples. Remove and clean IDs
-summary_tsv$ID <- ifelse(str_starts(summary_tsv$ID,"WA"),
-                         substr(summary_tsv$ID, 1, 9),
-                         summary_tsv$ID)
 
-summary_tsv$ID <- ifelse(str_starts(summary_tsv$ID, regex("[0-9]{4}")),
-                         substr(summary_tsv$ID, 1, 12),
-                         summary_tsv$ID)
-
-summary_tsv$ID <- ifelse(str_ends(summary_tsv$ID,"_T1"),
-                         str_remove(summary_tsv$ID, "_T1"),
-                         summary_tsv$ID)
-
+summary_tsv_cleaned<- summary_tsv %>%
+  mutate(ID = case_when(
+    str_starts(summary_tsv$ID, "WA") ~ substr(summary_tsv$ID, 1, 9),
+    str_starts(summary_tsv$ID, regex("[0-9]{4}")) ~ substr(summary_tsv$ID, 1, 12),
+    str_ends(summary_tsv$ID, "_T1") ~ str_remove(summary_tsv$ID, "_T1"),
+    TRUE ~ summary_tsv$ID
+  ))
 
 ##LOAD WABACTERIAMASTER_METADATA#
 wabacteriamaster_meta_df<- read.csv(wabacteriamaster_metadata)
